f5964fd0618a37b6a9fd3dfedb3f8db474f96387
Nat <bnat1@umbc.edu>
2016-05-10 00:07:02 -0400

    new landing

--- landing.php
+++ landing.php
@@ -28,7 +28,7 @@
 			</a><br /><br />
 				<?php
 					$allTaken = $_POST['taken'];
-					$classChoices = $taken200 = $taken300 = $taken400 = $takenMath =
+					$rec200 = $rec300 = $rec400 = $recMath = $recSci = $recStat = $taken200 = $taken300 = $taken400 = $takenMath =
 					$takenSci = $takenStat = array();
 					
 					/*categorize taken classes into their respective subjects and levels (for CMSC) */
@@ -53,179 +53,279 @@
 
 					/*CMSC Courses*/
 					if (empty($allTaken)){
-						array_push($classChoices, "CMSC 201", "MATH 151", "CHEM 101", "BIOL 141",
-									"PHYS 121", "GES 110", "GES 286", "SCI 100");
+						//array_push($classChoices, "CMSC 201", "MATH 151", "CHEM 101", "BIOL 141",
+									//"PHYS 121", "GES 110", "GES 286", "SCI 100");
+						array_push($rec200, "CMSC 201");
+						array_push($recMath, "MATH 151");
+						array_push($recSci, "CHEM 101", "BIOL 141", "PHYS 121", "GES 110", "GES 286", "SCI 100");
 					}
 					else if (!in_array ("CMSC 201", $taken200)){
-						array_push($classChoices, "CMSC 201");
+						//array_push($classChoices, "CMSC 201");
+						array_push($rec200, "CMSC 201");
 					}
 					else if (!in_array ("CMSC 202", $taken200)){
-						array_push($classChoices, "CMSC 202", "CMSC 352");
+						//array_push($classChoices, "CMSC 202", "CMSC 352");
+						array_push($rec200, "CMSC 202");
+						array_push($rec300, "CMSC 352");
 					}
 					else if (!in_array ("CMSC 203", $taken200)){
-						array_push($classChoices, "CMSC 203", "CMSC 232", "CMSC 304", "CMSC 352", "CMSC 484");
+						//array_push($classChoices, "CMSC 203", "CMSC 232", "CMSC 304", "CMSC 352", "CMSC 484");
+						array_push($rec200, "CMSC 203", "CMSC 232");
+						array_push($rec300, "CMSC 304", "CMSC 352");
+						array_push($rec400, "CMSC 484");
 					}
 					else {
-						array_push($classChoices, "CMSC 232", "CMSC 291", "CMSC 299", "CMSC 304", "CMSC 313", "CMSC 331", 
-										"CMSC 341", "CMSC 352", "CMSC 391", "CMSC 451", "CMSC 452", "CMSC 484");
+						//array_push($classChoices, "CMSC 232", "CMSC 291", "CMSC 299", "CMSC 304", "CMSC 313", "CMSC 331", 
+						//				"CMSC 341", "CMSC 352", "CMSC 391", "CMSC 451", "CMSC 452", "CMSC 484");
+						array_push($rec200, "CMSC 232", "CMSC 291", "CMSC 299");
+						array_push($rec300, "CMSC 304", "CMSC 313", "CMSC 331","CMSC 341", "CMSC 352", "CMSC 391");
+						array_push($rec400, "CMSC 451", "CMSC 452", "CMSC 484");
 					}
 					
 					if (in_array("CMSC 313", $taken300)){
-						array_push ($classChoices, "CMSC 411");
+						array_push ($rec400, "CMSC 411");
 						if (in_array("CMSC 341", $taken300)){
-							array_push ($classChoices, "CMSC 421");
+							array_push ($rec400, "CMSC 421");
 						}
 					}
 					if (in_array("CMSC 331", $taken300)){
-						array_push ($classChoices, "CMSC 432", "CMSC 433", "CMSC 473");
+						array_push ($rec400, "CMSC 432", "CMSC 433", "CMSC 473");
 					}
 					if (in_array("CMSC 341", $taken300)){
-						array_push ($classChoices, "CMSC 345", "CMSC 427", "CMSC 437", "CMSC 471",
+						array_push ($rec300, "CMSC 345");
+						array_push($rec400, "CMSC 427", "CMSC 437", "CMSC 471",
 									"CMSC 455", "CMSC 456", "CMSC 476", "CMSC 475", "CMSC 461", "CMSC 481");
 					}
 					
 					if (!empty($taken400)){
-						array_push($classChoices, "CMSC 447");
+						array_push($rec400, "CMSC 447");
 					}
 					if (in_array("CMSC 421", $taken400)){
-						array_push ($classChoices, "CMSC 426", "CMSC 483", "CMSC 487");
+						array_push ($rec400, "CMSC 426", "CMSC 483", "CMSC 487");
 					}
 					if (in_array("CMSC 471", $taken400)){
-						array_push ($classChoices, "CMSC 478", "CMSC 479");
+						array_push ($rec400, "CMSC 478", "CMSC 479");
 					}
 					if (in_array("CMSC 461", $taken400) && in_array("CMSC 481", $taken400)){
-						array_push ($classChoices, "CMSC 465", "CMSC 466");
+						array_push ($rec400, "CMSC 465", "CMSC 466");
 					}
 					if (in_array("CMSC 435", $taken400) && in_array("CMSC 471", $taken400)){
-						array_push ($classChoices, "CMSC 493");
+						array_push ($rec400, "CMSC 493");
 					}
 					if (in_array("CMSC 331", $taken300) && in_array("CMSC 341", $taken300)){
-						array_push ($classChoices, "CMSC 436");
+						array_push ($rec400, "CMSC 436");
 						
 						if (in_array("CMSC 313", $taken300)){
-							array_push ($classChoices, "CMSC 431");
+							array_push ($rec400, "CMSC 431");
 						}
 					}
 					if (in_array("CMSC 421", $taken400) && in_array("CMSC 481", $taken400)){
-						array_push ($classChoices, "CMSC 444");
+						array_push ($rec400, "CMSC 444");
 					}
 					
 					if (in_array("CMSC 447", $taken300)){
-						array_push ($classChoices, "CMSC 448");
+						array_push ($rec400, "CMSC 448");
 					}
 					
 					if (in_array("CMSC 471", $taken300)){
-						array_push ($classChoices, "CMSC 477", "CMSC 478", "CMSC 479");
+						array_push ($rec400, "CMSC 477", "CMSC 478", "CMSC 479");
 					}
 					
 					/* STATISTICS */
 					
 					if (in_array("MATH 151", $takenMath)){
-						array_push ($classChoices, "STAT 355");
+						array_push ($recStat, "STAT 355");
 					}
 					
 					/* MATH */
 					
 					if (!in_array("MATH 151", $takenMath)) {
-						array_push ($classChoices, "MATH 151");
+						array_push ($recMath, "MATH 151");
 					}
 					else {
-						array_push ($classChoices, "MATH 152", "MATH 221");
+						array_push ($recMath, "MATH 152", "MATH 221");
 					}
 					if (in_array("MATH 152", $takenMath)){
-						array_push ($classChoices, "CMSC 486");
+						array_push ($rec400, "CMSC 486");
 					}
 					if (in_array("MATH 221", $takenMath)){
 						if (in_array("CMSC 203", $taken200)){
-							array_push($classChoices, "CMSC 442", "CMSC 457");
+							array_push($rec400, "CMSC 442", "CMSC 457");
 						}
 						if (in_array("CMSC 341", $taken300) && in_array("STAT 355", $takenStat)){
-							array_push($classChoices, "CMSC 443");
+							array_push($rec400, "CMSC 443");
 						}
 						if (in_array("CMSC 341", $taken300) && in_array("MATH 152", $takenMath)){
-							array_push($classChoices, "CMSC 453");
+							array_push($rec400, "CMSC 453");
 						}
 						if (in_array("CMSC 313", $taken300) && in_array("CMSC 341", $taken300)){
-							array_push($classChoices, "CMSC 435");
+							array_push($rec400, "CMSC 435");
 						}
 						
 					}
 					if (in_array("STAT 355", $takenStat) && in_array("MATH 152", $takenMath)
 						&& in_array("CMSC 341", $taken300)){
-							array_push($classChoices, "CMSC 441");
+							array_push($rec400, "CMSC 441");
 					}
 					
 					/*Science Courses*/
 					if (empty($takenSci)){
-						array_push($classChoices, "CHEM 101", "BIOL 141", "PHYS 121",
+						array_push($recSci, "CHEM 101", "BIOL 141", "PHYS 121",
 									"GES 110", "GES 286", "SCI 100");
 					}
 					
 					if (!in_array("SCI 100", $takenSci)){
-						array_push($classChoices, "SCI 100");
+						array_push($recSci, "SCI 100");
 					}
 					
 					/*Chemistry */
 					if (!in_array("CHEM 101", $takenSci)){
-						array_push($classChoices, "CHEM 101");
+						array_push($recSci, "CHEM 101");
 					}
 					else if (!in_array("CHEM 102", $takenSci)){
-						array_push($classChoices, "CHEM 102", "CHEM 102L");
+						array_push($recSci, "CHEM 102", "CHEM 102L");
 					}
 					else {
-						array_push($classChoices, "CHEM 102L");
+						array_push($recSci, "CHEM 102L");
 					}
 					
 					/*Biology */
 					if (!in_array("BIOL 141", $takenSci)){
-						array_push($classChoices, "BIOL 141");
+						array_push($recSci, "BIOL 141");
 					}
 					else if (!in_array("BIOL 142", $takenSci)){
-						array_push($classChoices, "BIOL 142", "BIOL 142L");
+						array_push($recSci, "BIOL 142", "BIOL 142L");
 					}
 					else {
-						array_push($classChoices, "BIOL 142L");
+						array_push($recSci, "BIOL 142L");
 					}
 					
 					/*Physics */
 					if (!in_array("PHYS 121", $takenSci)){
-						array_push($classChoices, "PHYS 121");
+						array_push($recSci, "PHYS 121");
 					}
 					else if (!in_array("PHYS 122", $takenSci)){
-						array_push($classChoices, "PHYS 122", "PHYS 122L");
+						array_push($recSci, "PHYS 122", "PHYS 122L");
 					}
 					else {
-						array_push($classChoices, "PHYS 122L");
+						array_push($recSci, "PHYS 122L");
 					}
 					
 					/*Geography and Environmental Science */
 					if (!in_array("GES 110", $classChoices)){
-						array_push($classChoices, "GES 110");
+						array_push($recSci, "GES 110");
 					}
 					if (!in_array("GES 286", $classChoices)){
-						array_push($classChoices, "GES 286");
+						array_push($recSci, "GES 286");
 					}
 					
 					/*eliminates any redundant course choices*/
-					$classChoices = array_unique($classChoices);
-					
+					$rec200 = array_unique($rec200);
+					$rec300 = array_unique($rec300);
+					$rec400 = array_unique($rec400);
+					$recMath = array_unique($recMath);
+					$recSci = array_unique($recSci);
+					$recStat = array_unique($recStat);
+
 					if(!empty($allTaken)){
 						/*remove any possibly taken courses that ended up on their choice sheet */
-						$classChoices = array_diff($classChoices, $allTaken);	
+						$rec200 = array_diff($rec200, $allTaken);
+						$rec300 = array_diff($rec300, $allTaken);	
+						$rec400 = array_diff($rec400, $allTaken);
+						$recMath = array_diff($recMath, $allTaken);
+						$recSci = array_diff($recSci, $allTaken);
+						$recStat = array_diff($recStat, $allTaken);			
 					}
 					
 					/*put classes in alphabetical order */
-					sort($classChoices);
+					sort($rec200);
+					sort($rec300);
+					sort($rec400);
+					sort($recMath);
+					sort($recSci);
+					sort($recStat);
 					
 					/*display list of classes*/
-					foreach ($classChoices as $class1) {
-						echo $class1;
-						echo ("<br />");
+					if(!empty($rec200)){
+						echo "<h2 id='200'>200 Level CMSC Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($rec200 as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
 					}
-					echo ("<br />");
+					if(!empty($rec300)){
+						echo "<h2 id='300'>300 Level CMSC Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($rec300 as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
+					}
+					if(!empty($rec400)){
+						echo "<h2 id='400'>400 Level CMSC Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($rec400 as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
+					}
+					if(!empty($recMath)){
+						echo "<h2 id='math'>Math Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($recMath as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
+					}
+					if(!empty($recSci)){
+						echo "<h2 id='sci'>Science Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($recSci as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
+					}
+					if(!empty($recStat)){
+						echo "<h2 id='stat'>Statistics Classes</h2>\n
+							<div id='alignClasses'>
+							<div id='course'>";
+						foreach ($recStat as $class1) {
+							echo $class1;
+							echo ("<br />");
+						}
+						echo "</div>
+							</div>";
+					}
+
+
 				?>
 			<div> <! -- end scrollbar div -->
 		</div> <! -- end content div -->
 	</div> <! -- end container div -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("h2").click(function(){
+        $(this).next().slideToggle("fast");
+    });
+});
+</script>
+
 </body>
 </html>
